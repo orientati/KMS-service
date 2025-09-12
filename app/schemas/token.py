b8cf095568b273eb7740e29841cd5f866aea4bd3
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-09-12 13:32:46 +0200

    pulito template, creata struttura di base

---
+++ app/schemas/token.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+
+class TokenResponse(BaseModel):
+    verified: bool
+    expired: bool
+    expires_in: int
+    username: str
+    user_id: int
+    session_id: int
+
+
+class TokenCreate(BaseModel):
+    user_id: int
+    username: str
+    session_id: int
+    expires_in: int  # in minuti
